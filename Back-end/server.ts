524ffec6022336b142db3b137f1a6ceb7d654113
Hirotoshi Sato <hirotoshi.sato@nocodejapan.org>
2023-02-13 14:46:04 +0900

    first commit weather

---
+++ Back-end/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const app: express.Express = express();
+const port = 8000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('server successful');
+});
+
+app.get('/api', (req: express.Request, res: express.Response) => {
+    res.json([
+        {
+            weatherName: "晴れ",
+            ditail: "ダーツをした",
+        },
+        {
+            weatherName: "雨",
+            ditail: "勉強が捗らない",
+        },
+        {
+            weatherName: "曇り",
+            ditail: "やけどしたけど、ご飯は美味しかった",
+        },
+    ]);
+});
+
+app.listen(port, () => {
+    console.log(`port ${port} でサーバ起動中`);
+});
